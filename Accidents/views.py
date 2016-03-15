cdc9d30f1021dbd1437b3e25d8c628e2b2ec5b7a
makalaaneesh <makalaaneesh@yahoo.com>
2016-03-15 18:04:22 +0530

    template

--- Accidents/views.py
+++ Accidents/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+	accidents = Accident.objects.all();
+	for accident in accidents:
+		print type(accident.location)
+		print accident.location
+		accident.location = accident.location.encode('utf-8').replace("\n","")
+	return render(request, "Accidents/index2.djt", {'accidents':accidents})
